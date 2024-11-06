d7614802320e35216c23fbeabc5247a8a834b0ea
gourav <gouravgarg93145@gmail.com>
2024-11-07 03:24:32 +0530

    added video streaming

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-import logging, time, argparse
+import argparse
+import logging
+import configparser
+import sys
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', nargs=1, default=None)
 args = parser.parse_args()
 
 APP_DIR = args.d[0] if args.d != None else "./"
+CONFIGURATIONS = APP_DIR + 'configuration.ini'
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler(APP_DIR + 'logs/main app | ' + str(time.asctime()) + '.log'),
+        logging.FileHandler(APP_DIR + 'logs/main app ' + str(time.asctime()) + '.log'),
         logging.StreamHandler()
     ]
 )
 
-if __name__ == '__main__':
+config = configparser.ConfigParser()
+
+if len(config.read(CONFIGURATIONS)) == 0:
+    logging.error("Could Not Read Configurations File: " + CONFIGURATIONS)
+    sys.exit()
+
+DRONE_ID = config['drone']['id']
+HOST_IP = config['cloud-app']['ip']
+
+if __name__ == "__main__":
     logging.debug('DroneApp has started! Directory %s', APP_DIR)
